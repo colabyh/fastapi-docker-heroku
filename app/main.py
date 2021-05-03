b707c5813ba465d4a9be425932f3e2b1922f1d27
colabyh <80495057+colabyh@users.noreply.github.com>
2021-05-03 17:21:27 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
+
 from fastapi import FastAPI
+import recipe
+import base64
+from xor_base64 import xor, base64_recursive_decode
+from urllib.parse import unquote, quote
+
+charset = set(b'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r')
+charset_base64 = set(b'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/=')
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
