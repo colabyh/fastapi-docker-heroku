128aa1625f54e47b6e19892681162b9c9301d869
colabyh <80495057+colabyh@users.noreply.github.com>
2021-05-04 16:18:30 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -20,6 +20,8 @@ def brute_decode(to_decode, maxlen, results_to_show):
     to_decode = unquote(to_decode)
     to_decode = bytes(to_decode, 'ascii')
 
+    to_decode = base64_recursive_decode(to_decode)
+
     decoded = recipe.xor_brute(to_decode, int(maxlen), int(results_to_show))
     joint_list = []
     output_dict = {}
@@ -29,11 +31,151 @@ def brute_decode(to_decode, maxlen, results_to_show):
 
     added = 1
     for i in range(len(joint_list)):
-        if joint_list[i][-1] in output_dict.values():
-            continue
+        try:
+            printable = ""
+            
+            for j in joint_list[i][-1]:
+                if j in charset:
+                    printable += chr(j)
+                else:
+                   printable += "."
 
-        output_dict[str(added) + ") Key "] = (joint_list[i][1]).decode()
-        output_dict[str(added) + ") Decoded "] = joint_list[i][-1]
+            if (printable) in output_dict.values():
+                continue
+
+            output_dict[str(added) + ") Key "] = str(joint_list[i][1])
+            output_dict[str(added) + ") Decoded "] = printable
+        except:
+            pass
+
+        if added == int(results_to_show):
+            break
+
+        added += 1
+
+    return output_dict
+
+@app.get("/guess_xorlen/")
+def guess_xorlen(to_decode, results_to_show):
+
+    to_decode = unquote(to_decode)
+    to_decode = bytes(to_decode, 'ascii')
+
+    to_decode = base64_recursive_decode(to_decode)
+
+    decoded = recipe.xor_guess_keylen(to_decode, charset, int(results_to_show))
+
+    joint_list = []
+    output_dict = {}
+
+    if decoded is None:
+        return output_dict
+
+    for i in range(len(decoded)):
+        joint_list += decoded[i]
+
+    added = 1
+    for i in range(len(joint_list)):
+        try:
+            printable = ""
+            
+            for j in joint_list[i][-1]:
+                if j in charset:
+                    printable += chr(j)
+                else:
+                   printable += "."
+
+            if (printable) in output_dict.values():
+                continue
+
+            output_dict[str(added) + ") Key "] = str(joint_list[i][1])
+            output_dict[str(added) + ") Decoded "] = printable
+        except:
+            pass
+
+        if added == int(results_to_show):
+            break
+
+        added += 1
+
+    return output_dict
+
+@app.get("/fastxor/")
+def fastxor(to_decode, keylen, choose_charset, results_to_show):
+
+    to_decode = unquote(to_decode)
+    to_decode = base64_recursive_decode(to_decode)
+
+    if int(choose_charset) == 0:
+        decoded = recipe.xor_fast(to_decode, int(keylen), charset, int(results_to_show))
+
+    if int(choose_charset) == 1:
+        decoded = recipe.xor_fast(to_decode, int(keylen), charset_base64, int(results_to_show))
+    
+    joint_list = []
+    output_dict = {}
+
+    for i in range(len(decoded)):
+        joint_list += decoded[i]
+
+    added = 1
+    for i in range(len(joint_list)):
+        try:
+            printable = ""
+            
+            for j in joint_list[i][-1]:
+                if j in charset:
+                    printable += chr(j)
+                else:
+                   printable += "."
+
+            if (printable) in output_dict.values():
+                continue
+
+            output_dict[str(added) + ") Key "] = str(joint_list[i][1])
+            output_dict[str(added) + ") Decoded "] = printable
+        except:
+            pass
+
+        if added == int(results_to_show):
+            break
+
+        added += 1
+
+    return output_dict
+
+@app.get("/magic/")
+def magic(to_decode, keylen, results_to_show, early_stop):
+    
+    to_decode = unquote(to_decode)
+
+    decoded = recipe.auto_deobf(to_decode, int(keylen), int(results_to_show), int(early_stop), int(early_stop))
+
+    joint_list = []
+    output_dict = {}
+
+    for i in range(len(decoded)):
+        joint_list += decoded[i]
+
+    added = 1
+    for i in range(len(joint_list)):
+
+        try:
+            printable = ""
+            
+            for j in joint_list[i][-1]:
+                if j in charset:
+                    printable += chr(j)
+                else:
+                   printable += "."
+
+            if (printable) in output_dict.values():
+                continue
+
+            output_dict[str(added) + ") Key "] = str(joint_list[i][1])
+            output_dict[str(added) + ") Decoded "] = printable
+        except:
+            pass
 
         if added == int(results_to_show):
             break
